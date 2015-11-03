174d51c73e033dd03195538abcd8e4b1f1fecf45
Brian Woodward <brian.woodward@gmail.com>
2015-11-02 19:33:12 -0500

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * match-event <https://github.com/doowb/match-event>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
